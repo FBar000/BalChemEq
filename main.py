1c6ce4ceef455796f02b6d532493d6e98c3ebda8
unknown <barrera.federico@outlook.com>
2025-01-25 00:53:23 -0500

    demonstrating usage of BCE features

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import src.BCE
+import src.BCE as BCE
 
+equation_string = 'H2 + O2 : H2O'
+
+# return balanced equation
+print(BCE.balanceChemicalEquation(equation_string))
+
+# return solutions as an array
+print(BCE.findBalancingCoefficients(equation_string))
+
+# demonstrate intermediate calculations in `BCESteps.txt`
+BCE.writeBCESteps(equation_string)
